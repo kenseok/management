74f4b148175bbaf682b250723e167127ebafb4dc
kenseok <kenseok@gmail.com>
2020-11-09 01:18:43 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- client/README.md
+++ client/README.md
@@ -1,3 +1,13 @@
 ## Management System
 - 本プロジェクトはReactで作られた顧客管理システムです。
 - 본 프로젝트는 React로 만들어진 고객 관리 시스템입니다.
+
+
+- Node.js
+- node.js express
+- REST API Module
+- AWS RDS(database db)
+- mysql
+
+-tool
+- sequel pro
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');//데이터베이스에서 JSON 파일 읽어오기
+const conf = JSON.parse(data); //해당환경설정데이터파싱
+const mysql = require('mysql'); //mysql불러오기
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '홍길동이',
-            'birthday' : '961212',
-            'gender' : '남자',
-            'job' : '대학생'
-        },
-        {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '나동빈',
-            'birthday' : '900512',
-            'gender' : '남자',
-            'job' : '프로그래머'
-        },
-        {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '수요일',
-            'birthday' : '960312',
-            'gender' : '남자',
-            'job' : '벡수'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
