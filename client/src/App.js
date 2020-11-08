cc1d3e120a3798d7bf43615c458ea80dbbbfc273
kenseok <kenseok@gmail.com>
2020-11-09 03:07:47 +0900

    Add Customer Delete Module

--- client/src/App.js
+++ client/src/App.js
@@ -77,11 +77,12 @@ class App extends Component {
                 <TableCell>BIRTHDAY</TableCell>
                 <TableCell>SEX</TableCell>
                 <TableCell>JOB</TableCell>
+                <TableCell>SETTING</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {this.state.customers ? this.state.customers.map(c => { 
-                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); 
+                return ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); 
               }) : 
               <TableRow>
                 <TableCell colSpan="6" align="center">
